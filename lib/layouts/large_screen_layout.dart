07e33f30eb6cdbd25245045c803c1afc1fecf61e
Pol de Visser <p.devisser@bxter.nl>
2021-07-09 12:54:48 +0200

    implemented sliver appbar and sliverlist on large screen

--- lib/layouts/large_screen_layout.dart
+++ lib/layouts/large_screen_layout.dart
@@ -9,37 +9,114 @@ class LargeScreenLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size _screenSize = MediaQuery.of(context).size;
+    final double _containerHeightPercentage = 0.6;
+    final double _containerHeight =
+        _screenSize.height * _containerHeightPercentage;
+    final double _containerWidthPercentage = 0.8;
+    final double _containerWidth =
+        _screenSize.width * _containerWidthPercentage;
+
     return Column(
       children: [
-        TopBarDesktop(screenSize: _screenSize),
+        
+
+        // TODO implement slivers
         Expanded(
-          child: Center(
-            child: Container(
-              height: _screenSize.height * 0.4,
-              width: _screenSize.width * 0.4,
-              // color: Theme.of(context).accentColor,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(height: 100, width: 100, color: blue2),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Container(
-                      height: 30,
-                      width: 30,
-                      color: blue3,
-                    ),
-                  ),
-                  Container(
-                    height: 100,
-                    width: 100,
-                    color: blue4,
-                  ),
-                ],
+          child: CustomScrollView(
+            slivers: [
+              TopBarDesktop(screenSize: _screenSize),
+              // SliverAppBar(
+              //   backgroundColor: Colors.black,
+              //   title: Text(
+              //     'Portfolio website',
+              //     style: Theme.of(context).textTheme.headline6,
+              //   ),
+              //   floating: true,
+              //   leading: Icon(
+              //     Icons.change_history,
+              //     size: 32,
+              //     color: Theme.of(context).accentColor,
+              //   ),
+              //   actions: [
+              //     TextButton(
+              //       onPressed: () {},
+              //       child: Text("tab 1",
+              //           style: Theme.of(context).textTheme.headline6),
+              //     ),
+              //     TextButton(
+              //       onPressed: () {},
+              //       child: Text("tab 2",
+              //           style: Theme.of(context).textTheme.headline6),
+              //     ),
+              //     TextButton(
+              //       onPressed: () {},
+              //       child: Text("tab 3",
+              //           style: Theme.of(context).textTheme.headline6),
+              //     ),
+              //     TextButton(
+              //       onPressed: () {},
+              //       child: Text("tab 4",
+              //           style: Theme.of(context).textTheme.headline6),
+              //     ),
+              //   ],
+              // ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    // Return whatever you want here
+                    return Container(
+                      margin: EdgeInsets.symmetric(
+                          vertical: _screenSize.height *
+                              (1 - _containerHeightPercentage) /
+                              4,
+                          horizontal: _screenSize.width *
+                              (1 - _containerWidthPercentage) /
+                              2),
+                      color: Colors.teal[100 * (index % 9 + 1)],
+                      height: _containerHeight,
+                      width: _containerWidthPercentage,
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Item $index",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    );
+                  },
+                  childCount: 300,
+                ),
               ),
-            ),
+            ],
           ),
-        ),
+        )
+
+        // Expanded(
+        //   child: Center(
+        //     child: Container(
+        //       height: _screenSize.height * 0.4,
+        //       width: _screenSize.width * 0.4,
+        //       // color: Theme.of(context).accentColor,
+        //       child: Row(
+        //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //         children: [
+        //           Container(height: 100, width: 100, color: blue2),
+        //           Align(
+        //             alignment: Alignment.bottomCenter,
+        //             child: Container(
+        //               height: 30,
+        //               width: 30,
+        //               color: blue3,
+        //             ),
+        //           ),
+        //           Container(
+        //             height: 100,
+        //             width: 100,
+        //             color: blue4,
+        //           ),
+        //         ],
+        //       ),
+        //     ),
+        //   ),
+        // ),
       ],
     );
   }
--- lib/widgets/appbars/top_bar_desktop.dart
+++ lib/widgets/appbars/top_bar_desktop.dart
@@ -4,47 +4,41 @@ import 'package:web_portfolio/constants/tabs.dart';
 class TopBarDesktop extends StatelessWidget {
   final Size screenSize;
 
-  const TopBarDesktop({ Key? key, required this.screenSize }) : super(key: key);
+  const TopBarDesktop({Key? key, required this.screenSize}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-            height: screenSize.height * 0.14,
-            width: screenSize.width,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    // Center icon in iconbutton
-                    padding: const EdgeInsets.only(),
-                    icon: Icon(
-                      Icons.change_history,
-                      size: 32,
-                      color: Theme.of(context).accentColor,
-                    ),
-                  ),
-                  Spacer(),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 8,
-                      ),
-                      child: DefaultTabController(
-                        length: tabs.length,
-                        child: TabBar(
-                          indicatorColor: Theme.of(context).accentColor,
-                          indicatorSize: TabBarIndicatorSize.label,
-                          onTap: null,
-                          tabs: tabs,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
+    // TODO make scrolling in sliverappbar smooth on web
+    return SliverAppBar(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      title: Text(
+        'Portfolio website',
+        style: Theme.of(context).textTheme.headline6,
+      ),
+      floating: true,
+      leading: Icon(
+        Icons.change_history,
+        size: 32,
+        color: Theme.of(context).accentColor,
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {},
+          child: Text("tab 1", style: Theme.of(context).textTheme.headline6),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Text("tab 2", style: Theme.of(context).textTheme.headline6),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Text("tab 3", style: Theme.of(context).textTheme.headline6),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: Text("tab 4", style: Theme.of(context).textTheme.headline6),
+        ),
+      ],
+    );
   }
 }
